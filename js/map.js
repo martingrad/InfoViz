a33cb4bf99d9088e02f7ea4f5b288459e25cf49f
Emma Forsling <emma.forsling.parborg@gmail.com>
2015-02-20 10:11:26 +0100

    Added basically the code from lab1, with the map-structure and default look

---
+++ js/map.js
@@ -0,0 +1,137 @@
+function map(){
+
+    var zoom = d3.behavior.zoom()
+        .scaleExtent([1, 8])
+        .on("zoom", move);
+
+    var mapDiv = $("#map");
+
+    var margin = {top: 20, right: 20, bottom: 20, left: 20},
+        width = mapDiv.width() - margin.right - margin.left,
+        height = mapDiv.height() - margin.top - margin.bottom;
+
+    //initialize color scale
+    var fill = d3.scale.log()
+        .domain([10, 500])
+        .range(["brown", "steelblue"]);
+
+    var countryColorScale = d3.scale.category20();
+    
+    //initialize tooltip
+    // add the tooltip area to the webpage
+    var tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    var projection = d3.geo.mercator()
+        .center([50, 60 ])
+        .scale(250);
+
+    var svg = d3.select("#map").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .call(zoom);
+
+    var path = d3.geo.path()
+        .projection(projection);
+
+    g = svg.append("g");
+
+    // d3.json("data/world-topo.json",function(error, world){
+    //     console.log(topojson.feature(world,world.objects.countries));
+    // });
+    // load data and draw the map
+    d3.json("data/swe-topo.json",function(error, sweden) {
+        //myconfig = JSON.parse(data.toString('utf8').replace(/^\uFEFF/, ''));
+        console.log(topojson.feature(sweden,sweden.objects.swe_mun).features);
+        
+        var counties = topojson.feature(sweden, sweden.objects.swe_mun).features;
+
+        //console.log(countries.path);
+        //load summary data
+        //...
+        draw(counties, sp1.getData());
+        
+    });
+
+    function draw(countries, data)
+    {
+        var country = g.selectAll(".country").data(countries);
+        var id = g.selectAll(".country").data(countries);
+        
+        //initialize a color country object
+        //console.log(id);
+
+        var color = "#800026";
+        var cc = {Country: country, Color: color};
+        //console.log(cc.Color);
+    
+
+        country.enter().insert("path")
+            .attr("class", "country")
+            .attr("d", path)
+            .attr("id", function(d) { return d.id; })
+            .attr("title", function(d) { return d.properties.name; })
+            //country color
+            .style("fill", function(d) 
+                {
+                    return countryColorScale(d.properties.name);
+                })
+                        
+            //...
+            //tooltip
+            .on("mousemove", function(d, i) {
+                tooltip.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+    
+                tooltip.html(d.properties.name)
+                    .style("left", (d3.event.pageX + 5) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+
+            })
+            .on("mouseout", function(d) {
+                tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            })
+
+            //selection
+            .on("click",  function(d) {
+                selFeature(d);
+            });
+    }
+
+    
+    
+    //zoom and panning method
+    function move() {
+
+        var t = d3.event.translate;
+        var s = d3.event.scale;
+        
+
+        zoom.translate(t);
+        g.style("stroke-width", 1 / s).attr("transform", "translate(" + t + ")scale(" + s + ")");
+
+    }
+    
+    // detta är vår funktion, fanns ej med i orginal koden
+    this.selectCountry = function(value){
+        // d3.select("#map").selectAll("path").style("opacity", function(d){if(d.properties.name != value) return 0.3;});
+        // d3.select("#map").selectAll("path").style("fill", function(d){ if(d.properties.name == value) return "#ff1111"; else return countryColorScale(d.properties.name);});//
+    };
+
+    // this.deselectCountry = function(){
+    //     d3.select("#map").selectAll("path").style("opacity", function(d){ return 0.9;});
+    //     d3.select("#map").selectAll("path").style("fill", function(d){ return countryColorScale(d.properties.name);});//
+    // }
+
+    //method for selecting features of other components
+    function selFeature(value){
+        // map.selectCountry(value.properties.name);
+        // sp1.selectDot(value.properties.name);
+        // pc1.selectLine(value.properties.name);
+    }
+}
+
---
+++ js/pc.js
@@ -0,0 +1,194 @@
+function pc(){
+
+    var self = this; // for internal d3 functions
+
+    var pcDiv = $("#pc");
+
+    var margin = [30, 10, 10, 10],
+        width = pcDiv.width() - margin[1] - margin[3],
+        height = pcDiv.height() - margin[0] - margin[2];
+
+    
+    //initialize color scale
+    var countryColorScale = d3.scale.category20();
+    
+    //initialize tooltip
+    var tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    var x = d3.scale.ordinal().rangePoints([0, width], 1),      //range between 0 and width, with padding 1
+        y = {};
+        
+
+    var line = d3.svg.line(),
+        axis = d3.svg.axis().orient("left"),
+        background,
+        foreground;
+
+    var svg = d3.select("#pc").append("svg:svg")
+        .attr("width", width + margin[1] + margin[3])
+        .attr("height", height + margin[0] + margin[2])
+        .append("svg:g")
+        .attr("transform", "translate(" + margin[3] + "," + margin[0] + ")");
+
+
+    //Load data
+    // d3.csv("data/OECD-better-life-index-hi.csv", function(data) {
+
+    //     self.data = data;
+        
+    //     // Extract the list of dimensions and create a scale for each.
+    //     // För att läsa endast en liten pluttig rad. Kan detta göras på något annat sätt annars bojkottar vi javascript i framtiden.
+    //     // var headers = [];
+    //     // var BreakException= {};
+    //     // try {
+    //     //     data.forEach(function(row) {
+    //     //         headers = Object.keys(row);
+    //     //         if(true) throw BreakException;
+    //     //     });
+    //     // }
+    //     // catch(e) {
+    //     //     if (e!==BreakException) throw e;
+    //     // }
+    //     // //console.log(headers);
+
+    //     // console.log(self.data[0][headers[0]]);
+
+    //     // varje stapel är en variabel
+    //     // varje linje är ett land
+        
+    //     // console.log(data[0]);
+
+    //     x.domain(dimensions = d3.keys(data[0]).filter(function(d) {
+    //         //console.log(d3.keys[4]);
+    //         return d != "Country" && (y[d] = d3.scale.linear()
+    //             .domain(d3.extent(data, function(p) { return +p[d]; }))
+    //             .range([height, 0]));
+    //     }));
+
+    //     // // Extract the list of dimensions and create a scale for each.
+    //     // x.domain(dimensions = d3.keys(cars[0]).filter(function(d) {
+    //     //     return d != "name" && (y[d] = d3.scale.linear()
+    //     //     .domain(d3.extent(cars, function(p) { return +p[d]; }))
+    //     //     .range([height, 0]));
+    //     // }));
+
+
+    //     draw();
+    // });
+
+    // function draw(){
+    //     // Add grey background lines for context.
+    //     background = svg.append("svg:g")
+    //         .attr("class", "background")
+    //         .selectAll("path")
+    //         //add the data and append the path 
+    //         .data(self.data)
+    //         .enter().append("path")
+    //         .attr("d",path)
+    //         .on("mousemove", function(d){})
+    //         .on("mouseout", function(){})
+    //         .on("click",  function(d) {
+    //             selFeature(d);
+    //         })
+
+    //       //   // Add grey background lines for context.
+    //       // background = svg.append("g")
+    //       //     .attr("class", "background")
+    //       //   .selectAll("path")
+    //       //     .data(cars)
+    //       //   .enter().append("path")
+    //       //     .attr("d", path);
+
+    //     // Add blue foreground lines for focus.
+    //     foreground = svg.append("svg:g")
+    //         .attr("class", "foreground")
+    //         .selectAll("path")
+    //         //add the data and append the path
+    //         .data(self.data)
+    //         .enter().append("path")
+    //         .attr("d",path)                     //attribute d - path data
+    //         .style("stroke", function(d, i){ return countryColorScale(d["Country"]);})
+    //         .on("mousemove", function(d, i) {
+    //             tooltip.transition()
+    //                 .duration(200)
+    //                 .style("opacity", .9);
+    
+    //             tooltip.html(d["Country"])
+    //                 .style("left", (d3.event.pageX + 5) + "px")
+    //                 .style("top", (d3.event.pageY - 28) + "px");  
+    //         })
+    //         .on("mouseout", function(d) {
+    //             tooltip.transition()
+    //                 .duration(500)
+    //                 .style("opacity", 0);
+    //         })
+    //         .on("click",  function(d) {
+    //             selFeature(d);
+    //         });
+
+    //     // Add a group element for each dimension.
+    //     var g = svg.selectAll(".dimension")
+    //         .data(dimensions)
+    //         .enter().append("svg:g")
+    //         .attr("class", "dimension")
+    //         .attr("transform", function(d) { return "translate(" + x(d) + ")"; });
+            
+    //     // Add an axis and title.
+    //     g.append("svg:g")
+    //         .attr("class", "axis")
+    //         //add scale
+    //         .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
+    //         .append("svg:text")
+    //         .attr("text-anchor", "middle")
+    //         .attr("y", -9)
+    //         //.text(String);
+    //         .text(function(d) { return d; });
+
+    //     // Add and store a brush for each axis.
+    //     g.append("svg:g")
+    //         .attr("class", "brush")
+    //         .each(function(d) { d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brush", brush)); })
+    //         .selectAll("rect")
+    //         .attr("x", -8)
+    //         .attr("width", 16);
+    // }
+
+    // // Returns the path for a given data point.
+    // function path(d) {
+    //     return line(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
+    // }
+
+    // // Handles a brush event, toggling the display of foreground lines.
+    // function brush() {
+    //     var actives = dimensions.filter(function(p) {return !y[p].brush.empty(); }),    // de axlar som används
+    //         extents = actives.map(function(p) { return y[p].brush.extent(); });         // de intervall som är markerade
+    //     foreground.style("display", function(d) {
+    //                                                                    // d = alla land-objekt
+    //         return actives.every(function(p, i) {                                       // p = namn på aktiv axel,  i = index för aktiv axel
+                
+    //             return extents[i][0] <= d[p] && d[p] <= extents[i][1];                  // extents[i][0] = nedre gräns, extents[i][1] = övre gräns
+    //         }) ? null : "none";                                                         // d[p] = all data på aktuell axel
+    //     });
+    // }
+
+    // //method for selecting the pololyne from other components	
+    // this.selectLine = function(value){          //skickar med value som är landet
+    //     d3.select("#pc").selectAll("path").style("opacity", function(d){if(d["Country"] != value) return 0.1;});
+    //     d3.select("#pc").selectAll("path").style("stroke", function(d){ if(d["Country"] == value) return "#ff1111"; else return countryColorScale(d["Country"]);});//function(d){ if(d["Country"] == value) return "#ff1111";});
+    // };
+
+    // this.deselectLine = function(){
+    //     d3.select("#pc").selectAll("path").style("opacity", function(d){ return 0.9;});
+    //     d3.select("#pc").selectAll("path").style("stroke", function(d){ return countryColorScale(d["Country"]);});
+    // }
+    
+    // //method for selecting features of other components
+    // function selFeature(value){
+    //     pc1.selectLine(value.Country);
+    //     sp1.selectDot(value.Country);
+    //     map.selectCountry(value.Country);
+    // };
+
+}
