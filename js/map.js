4e162c312826f516c874f61025fe0d0eabf3a6c7
Martin Gråd <martingrad@hotmail.com>
2015-02-24 14:50:14 +0100

    Simple coloring of map.

--- js/map.js
+++ js/map.js
@@ -7,48 +7,36 @@ function map(){
     var mapDiv = $("#map");
 
     var margin = {top: 20, right: 20, bottom: 20, left: 20},
-        width  = mapDiv.width() - margin.right - margin.left,
-        height = mapDiv.height() - margin.top - margin.bottom;
+        width  = mapDiv.width(),
+        height = mapDiv.height();
 
-    //initialize color scale
+    // initialize color scale
     var fill = d3.scale.log()
         .domain([10, 500])
         .range(["brown", "steelblue"]);
 
     var countryColorScale = d3.scale.category20();
     
-    //initialize tooltip
+    // initialize tooltip
     // add the tooltip area to the webpage
     var tooltip = d3.select("body").append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
 
-    /*var projection = d3.geo.mercator()
-        .center([20, 60])
-        .scale(2500);*/
-
-    /*var projection = d3.geo.satellite()
-        .distance(1.1)
-        .scale(5500)
-        .rotate([0, 0, 0])
-        .tilt(25)
-        .center([20, 60])
-        //.clipAngle(Math.acos(1 / 1.1) * 180 / Math.PI - 1e-6)
-        .precision(.1);*/
-
     var projection = d3.geo.satellite()
         .distance(1.1)
-        .scale(3000)
+        .scale(3200)
         .rotate([165.00, -125.0, 180.0])
-        .center([100, 40])
+        .center([80, 40])
         .tilt(15)
         //.clipAngle(Math.acos(1 / 1.1) * 180 / Math.PI - 1e-6)
         .precision(.1);
 
     /*var graticule = d3.geo.graticule()
-    .extent([[80, 90], [-20, 50]])
-    .step([3, 3]);
+                    .extent([[80, 90], [-20, 50]])
+                    .step([3, 3]);
     */
+
     var path = d3.geo.path()
         .projection(projection);
 
@@ -74,8 +62,6 @@ function map(){
     // load data and draw the map
     d3.json("data/swe-topo.json",function(error, sweden) {
         //myconfig = JSON.parse(data.toString('utf8').replace(/^\uFEFF/, ''));
-        console.log(topojson.feature(sweden,sweden.objects.swe_mun).features);
-        
         var counties = topojson.feature(sweden, sweden.objects.swe_mun).features;
 
         //console.log(countries.path);
@@ -87,6 +73,8 @@ function map(){
 
     function draw(countries, data)
     {
+        //console.log(countries);
+
         var country = g.selectAll(".country").data(countries);
         var id = g.selectAll(".country").data(countries);
         
@@ -96,19 +84,26 @@ function map(){
         var color = "#800026";
         var cc = {Country: country, Color: color};
         //console.log(cc.Color);
+        //var alpha;
+        //var coordinateY;
     
-
+        //var languageScale = d3.scale.category10();
         country.enter().insert("path")
             .attr("class", "country")
             .attr("d", path)
             .attr("id", function(d) { return d.id; })
             .attr("title", function(d) { return d.properties.name; })
             //country color
-            .style("fill", function(d) 
+            .style("fill", function(d)
                 {
-                    return countryColorScale(d.properties.name);
+                    var coordinateY = d.geometry.coordinates[0][0][1]
+                    if(d.geometry.type == "MultiPolygon")
+                        coordinateY = coordinateY[1];
+                    // fulhack var det här! manuell normalisering... icke bra!
+                    var alpha = 1 - (coordinateY - 55) / 14;
+                    return "rgba(" + [50 , 50, 175, alpha] + ")";
                 })
-                        
+            
             //...
             //tooltip
             .on("mousemove", function(d, i) {
@@ -149,8 +144,18 @@ function map(){
     
     // detta är vår funktion, fanns ej med i orginal koden
     this.selectCountry = function(value){
-        // d3.select("#map").selectAll("path").style("opacity", function(d){if(d.properties.name != value) return 0.3;});
-        // d3.select("#map").selectAll("path").style("fill", function(d){ if(d.properties.name == value) return "#ff1111"; else return countryColorScale(d.properties.name);});//
+        //d3.select("#map").selectAll("path").style("opacity", function(d){if(d.properties.name != value) return 0.3;});
+        d3.select("#map").selectAll("path").style("fill", function(d){
+            if(d.properties.name == value)
+                return "#b3a";
+            else
+                var coordinateY = d.geometry.coordinates[0][0][1]
+                if(d.geometry.type == "MultiPolygon")
+                    coordinateY = coordinateY[1];
+                // fulhack var det här! manuell normalisering... icke bra!
+                var alpha = 1 - (coordinateY - 55) / 14;
+                return "rgba(" + [50 , 50, 175, alpha] + ")";
+            });
     };
 
     // this.deselectCountry = function(){
@@ -160,7 +165,7 @@ function map(){
 
     //method for selecting features of other components
     function selFeature(value){
-        // map.selectCountry(value.properties.name);
+        map.selectCountry(value.properties.name);
         // sp1.selectDot(value.properties.name);
         // pc1.selectLine(value.properties.name);
     }
