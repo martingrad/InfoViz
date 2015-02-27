46f3efe664e6f660163dd60fb1bd825b7b68be32
Emma Forsling <emma.forsling.parborg@gmail.com>
2015-02-27 11:30:17 +0100

    Added movable parallell coordinates, not ready to push yet since there are some errors still lurking around. (concering the axis scaling)

--- js/pc.js
+++ js/pc.js
@@ -1,5 +1,6 @@
 function pc(){
 
+    //
     var self = this; // for internal d3 functions
 
     var pcDiv = $("#pc");
@@ -18,7 +19,8 @@ function pc(){
         .style("opacity", 0);
 
     var x = d3.scale.ordinal().rangePoints([0, width], 1),      //range between 0 and width, with padding 1
-        y = {};
+        y = {}
+        dragging = {};                  //ny
         
 
     var line = d3.svg.line(),
@@ -32,50 +34,159 @@ function pc(){
         .append("svg:g")
         .attr("transform", "translate(" + margin[3] + "," + margin[0] + ")");
 
+    // Ny parallell koordinat
+    d3.csv("data/databaosen.csv", function(error, data) {
 
 
-    var selectedObjectOnYAxis = $("#selectScatterPlotYAxis option:selected").val();
-    var selectedObjectOnXAxis = $("#selectScatterPlotXAxis option:selected").val();
-    var selectedYear = $("#selectYear option:selected").text();
+        // Extract the list of dimensions and create a scale for each.
+        x.domain(dimensions = d3.keys(data[0]).filter(function(d) {
+            return d != "region" && (y[d] = d3.scale.linear()
+                .domain(d3.extent(data, function(p) { 
+                    return +p[d]; 
+                }))
+                .range([height, 0]));
+        }));
+
+        // Add grey background lines for context.
+        background = svg.append("g")
+            .attr("class", "background")
+            .selectAll("path")
+            .data(data)
+            .enter().append("path")
+            .attr("d", path);
+
+        // Add blue foreground lines for focus.
+        foreground = svg.append("g")
+            .attr("class", "foreground")
+            .selectAll("path")
+            .data(data)
+            .enter().append("path")
+            .attr("d", path);
+
+        // Add a group element for each dimension.
+        var g = svg.selectAll(".dimension")
+            .data(dimensions)
+            .enter().append("g")
+            .attr("class", "dimension")
+            .attr("transform", function(d) { 
+                return "translate(" + x(d) + ")"; 
+            })
+            .call(d3.behavior.drag()
+            .origin(function(d) { 
+                return {x: x(d)}; 
+            })
+            .on("dragstart", function(d) {
+                dragging[d] = x(d);
+                background.attr("visibility", "hidden");
+            })
+            .on("drag", function(d) {
+                dragging[d] = Math.min(width, Math.max(0, d3.event.x));
+                foreground.attr("d", path);
+                dimensions.sort(function(a, b) { 
+                    return position(a) - position(b); 
+                });
+                x.domain(dimensions);
+                g.attr("transform", function(d) { 
+                    return "translate(" + position(d) + ")"; 
+                })
+                //.style("cursor", "url(https://mail.google.com/mail/images/2/closedhand.cur) 8 8, move;");
+                
+            })
+            .on("dragend", function(d) {
+                delete dragging[d];
+                transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
+                transition(foreground).attr("d", path);
+                background
+                    .attr("d", path)
+                    .transition()
+                    .delay(500)
+                    .duration(0)
+                    .attr("visibility", null);
+            }));
+
+        // Add an axis and title.
+        g.append("g")
+            .attr("class", "axis")
+            .each(function(d) { 
+                d3.select(this).call(axis.scale(y[d])); 
+            })
+            .append("text")
+            .style("text-anchor", "middle")
+            .attr("y", -9)
+            .text(function(d) { 
+                return d; 
+            })
+            .style("cursor", "pointer")
+            .style("cursor","hand")
+            .style("cursor" ,"-webkit-grabbing");
+
+        // Add and store a brush for each axis.
+        g.append("g")
+            .attr("class", "brush")
+            .each(function(d) {
+                d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brushstart", brushstart).on("brush", brush));
+            })
+            .selectAll("rect")
+            .attr("x", -8)
+            .attr("width", 16);
+        });
+
+        function position(d) {
+            var v = dragging[d];
+            return v == null ? x(d) : v;
+        }
+
+        function transition(g) {
+            return g.transition().duration(500);
+        }
+
+        // Returns the path for a given data point.
+        function path(d) {
+            return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
+        }
+
+        function brushstart() {
+            d3.event.sourceEvent.stopPropagation();
+        }
+
+        // Handles a brush event, toggling the display of foreground lines.
+        function brush() {
+            var actives = dimensions.filter(function(p) { 
+                    return !y[p].brush.empty(); 
+                }),
+                extents = actives.map(function(p) { 
+                    return y[p].brush.extent(); 
+                });
+            foreground.style("display", function(d) {
+                return actives.every(function(p, i) {
+                    return extents[i][0] <= d[p] && d[p] <= extents[i][1];
+                }) ? null : "none";
+            });
+        }
+
+
     
-    var valResultat;
-    var headers;
-    
-    console.log(selectedYear);
 
-    // Ladda in valresultatdata
-    d3.csv("data/Elections/Swedish_Election_" + selectedYear + ".csv", function(error,data) 
-    {
-        // valResultat.data = data;
-        // x.domain(headers = d3.keys(data[0]).filter(function(d) {
-        //      return d;
-        // }));
-        // console.log(headers);
-        // console.log(selectedObjectOnYAxis);
-        // console.log(selectedObjectOnXAxis);
-        // console.log(valResultat.data);
 
-    });
+
+    // // Ladda in valresultatdata
+    // d3.csv("data/Elections/Swedish_Election_" + selectedYear + ".csv", function(error,data) 
+    // {
+    //     // valResultat.data = data;
+    //     // x.domain(headers = d3.keys(data[0]).filter(function(d) {
+    //     //      return d;
+    //     // }));
+    //     // console.log(headers);
+    //     // console.log(selectedObjectOnYAxis);
+    //     // console.log(selectedObjectOnXAxis);
+    //     // console.log(valResultat.data);
+
+    // });
 
     // //Load data
     // d3.csv("data/Elections/Swedish_Election_2002.csv", function(data) {
 
     //     self.data = data;
-        
-    // //     // Extract the list of dimensions and create a scale for each.
-    // //     // För att läsa endast en liten pluttig rad. Kan detta göras på något annat sätt annars bojkottar vi javascript i framtiden.
-    // //     // var headers = [];
-    // //     // var BreakException= {};
-    // //     // try {
-    // //     //     data.forEach(function(row) {
-    // //     //         headers = Object.keys(row);
-    // //     //         if(true) throw BreakException;
-    // //     //     });
-    // //     // }
-    // //     // catch(e) {
-    // //     //     if (e!==BreakException) throw e;
-    // //     // }
-    // //     // //console.log(headers);
 
     // //     // console.log(self.data[0][headers[0]]);
 
